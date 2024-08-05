dce6d741f87f61ddd8a5c8d70bd7e82df21f782d
Shreyas Kumar <109721490+ShreyasKumar14@users.noreply.github.com>
2024-08-05 17:34:55 +0530

    commit/Shreyas-updated auth_users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -15,14 +15,21 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const user = {username:req.body.username,password:req.body.password} 
+  const result = users.find((i)=>{return i.username==user.username && i.password==user.password})
+
+  if(result!=undefined){
+    res.status(200).send("Customer logged in successfully")
+  }else{
+    res.status(400).send("User Not Found!")
+  }
+  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  books[req.params.isbn].reviews=req.query.review
+  res.status(200).send(`The review for the book with ISBN ${req.params.isbn} has been added/updated!`);
 });
 
 module.exports.authenticated = regd_users;
